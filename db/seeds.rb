2979900baca4495e49319e1a04a28e944b623598
Antoine Delprat <antoine.delprat.pro@gmail.com>
2023-05-24 16:15:03 +0200

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts 'Creating 10 fake restaurants...'
+
+10.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: "french",
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code
+  )
+  restaurant.save!
+end
+
+puts 'Finished!'
